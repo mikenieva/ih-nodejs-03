26a17e64158c4e17c2af008d4ae6ac3a8ce6c9e7
Mike Nieva <m@mikenieva.com>
2021-11-17 09:23:59 -0600

    Archivos base

---
+++ index.js
@@ -0,0 +1,39 @@
+// index.js
+
+// 1. IMPORTACIONES
+const express 		= require("express")
+const app			= express()
+
+require("dotenv").config()
+
+
+// 2. MIDDLEWARES
+app.use(express.static('public'))
+
+app.set("views", __dirname + "/views")
+app.set("view engine", "hbs")
+
+
+
+// 3. RUTAS
+app.get("/", (req, res) => {
+	res.render("index")
+})
+
+app.get("/players", (req, res) => {
+	res.render("players")
+})
+
+app.get("/teams", (req, res) => {
+	res.render("teams")
+})
+
+
+
+
+
+// 4. SERVIDOR
+app.listen(process.env.PORT, () => {
+	console.log(`Servidor activo en puerto ${process.env.PORT}`)
+})
+
